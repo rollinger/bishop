f5593c464c60456e190d95a919dfd639aedc7173
Philipp Rollinger <philipp.rollinger@gmail.com>
2014-01-05 14:25:18 +0100

    Semantic Kernel added

---
+++ lib/semantic_kernel.rb
@@ -0,0 +1,18 @@
+# encoding: UTF-8
+#
+# Semantic Kernel Implementation
+#
+# The library extends String Class adding semantic functionality
+
+module Noam
+	class Text 
+	end
+	class Word < String
+	end
+	class Number < Word
+	end
+	class Noun < Word
+	end
+	class Verb < Word
+	end
+end
